61f424ecbd8b0b86dd02f2d3ac61428482d38b78
samuele_lusito <samuelelusito@gmail.com>
2025-11-23 23:34:24 +0100

    add initial TicketBlock application with Maven setup and configuration

---
+++ src/main/java/com/ticketblock/TicketBlockApplication.java
@@ -0,0 +1,13 @@
+package com.ticketblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketBlockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketblock/TicketBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
