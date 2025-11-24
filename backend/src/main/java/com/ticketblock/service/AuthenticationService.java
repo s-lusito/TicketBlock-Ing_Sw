76f98b3c93b22a7438af1a0ee70c10ec82370a48
samuele_lusito <samuelelusito@gmail.com>
2025-11-24 21:20:39 +0100

    feat(be): implement AuthenticationService for user authentication and registration

---
+++ backend/src/main/java/com/ticketblock/service/AuthenticationService.java
@@ -0,0 +1,47 @@
+package com.ticketblock.service;
+
+import com.ticketblock.config.JwtService;
+import com.ticketblock.dto.Request.AuthenticationRequest;
+import com.ticketblock.dto.Request.RegisterRequest;
+import com.ticketblock.dto.Response.AuthenticationResponse;
+import com.ticketblock.entity.Role;
+import com.ticketblock.entity.User;
+import com.ticketblock.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword())); //autentico
+        var user =  userRepository.findByEmail(request.getEmail()).orElseThrow(()-> new UsernameNotFoundException("User not found")); //prelevo dal db
+        var jwt = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwt).build();
+
+    }
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        if (request.getRole().equals(Role.ADMIN)){
+            throw new IllegalArgumentException("You are not allowed to register as admin");
+        }
+        var user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .build();
+        userRepository.save(user);
+        var jwt = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwt).build();
+    }
+}
