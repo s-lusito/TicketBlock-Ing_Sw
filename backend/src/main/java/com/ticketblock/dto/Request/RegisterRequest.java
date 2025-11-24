29471cd31c50ec1d3622f71169c1e008b6fd61cc
samuele_lusito <samuelelusito@gmail.com>
2025-11-24 21:17:44 +0100

    feat(be): enhance RegisterRequest DTO with validation constraints

--- backend/src/main/java/com/ticketblock/dto/Request/RegisterRequest.java
+++ backend/src/main/java/com/ticketblock/dto/Request/RegisterRequest.java
@@ -1,9 +1,13 @@
 package com.ticketblock.dto.Request;
 
+import com.ticketblock.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * A data transfer object used for registering a new user.
@@ -16,8 +20,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "First name is required")
     private String firstName;
+    @NotBlank(message = "Last name is required")
     private String lastName;
+    @Email(message = "Not a valid email")
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is required")
     private String password;
+    @jakarta.validation.constraints.NotNull(message = "Role is required")
+    private Role role;
 }
