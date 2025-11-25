705b8a5cd565f8ae30849fdadf4f09a929a9f997
samuele_lusito <samuelelusito@gmail.com>
2025-11-25 23:22:39 +0100

    feat(be): implement GlobalExceptionHandler for centralized exception management

---
+++ backend/src/main/java/com/ticketblock/controller/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.ticketblock.controller;
+
+import com.ticketblock.dto.Response.ApiFieldError;
+import com.ticketblock.dto.Response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice //Response body + controller advice
+// @ControllerAdvice: questa classe ascolta tutte le eccezioni lanciate da qualsiasi controller
+// @Response Body: annotazione che si mette nei metodi affinchè i valori restituiti dai metodi vengono serializzati in JSON
+public class GlobalExceptionHandler {
+
+    //cattura in automatico questo tipo di eccezioni lanciate, senza bisogno del try catch
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<?> handleUsernameNotFoundException(UsernameNotFoundException exception) {
+        ErrorResponse errorResponse= ErrorResponse.builder().message(exception.getMessage()).build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+
+    // Gestisci anche eccezioni generiche
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGenericException(Exception exception) {
+        ErrorResponse errorResponse= ErrorResponse.builder().message("Internal Server Error").build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<ApiFieldError> fieldErrors = new ArrayList<>();
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            fieldErrors.add(
+                    ApiFieldError.builder()
+                            .field(fieldError.getField())
+                            .message(fieldError.getDefaultMessage())
+                            .build()
+            );
+        }
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse errorResponse = ErrorResponse.builder().message("Validation Failed").status(status.value()).errors(fieldErrors).build();
+        return ResponseEntity.status(status).body(errorResponse);
+
+
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException exception) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ErrorResponse errorResponse= ErrorResponse.builder().message("Authentication failed: Invalid email or password").status(status.value()).build();
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
+
+
+}
