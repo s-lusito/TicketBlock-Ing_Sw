2dc03d853babdadc4c1455288abe937507205e3b
samuele_lusito <samuelelusito@gmail.com>
2025-11-25 10:50:19 +0100

    feat(be): add AuthenticationController for user registration and authentication

---
+++ backend/src/main/java/com/ticketblock/controller/AuthenticationController.java
@@ -0,0 +1,33 @@
+package com.ticketblock.controller;
+
+import com.ticketblock.dto.Request.AuthenticationRequest;
+import com.ticketblock.dto.Request.RegisterRequest;
+import com.ticketblock.dto.Response.AuthenticationResponse;
+import com.ticketblock.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    //valid serve a validare i dati nel dto con le regole previste nel dto
+    //Se qualche regola non è rispettata Spring blocca l’esecuzione del metodo e restituisce un 400 Bad Request con i messaggi di errore delle annotazioni.
+    //request body legge il body della richiesta http e mappa i dati del json sull'oggetto RegisterRequest
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> registerUser(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@Valid @RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+
+}
