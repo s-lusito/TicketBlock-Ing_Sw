87a2677cbeb5233b151ba877b5c479cc312981fd
samuele_lusito <samuelelusito@gmail.com>
2025-11-30 19:07:09 +0100

    feat(be): add validation annotations for email and password in AuthenticationRequest

--- backend/src/main/java/com/ticketblock/dto/Request/AuthenticationRequest.java
+++ backend/src/main/java/com/ticketblock/dto/Request/AuthenticationRequest.java
@@ -1,5 +1,7 @@
 package com.ticketblock.dto.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +18,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Not a valid email")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 
 }
