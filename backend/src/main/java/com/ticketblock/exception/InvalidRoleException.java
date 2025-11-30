89a169d8518596aac5b07a32c71efcee7c17f18c
samuele_lusito <samuelelusito@gmail.com>
2025-11-30 19:30:37 +0100

    feat(be): update error message in InvalidRoleException to reflect new role names

--- backend/src/main/java/com/ticketblock/exception/InvalidRoleException.java
+++ backend/src/main/java/com/ticketblock/exception/InvalidRoleException.java
@@ -2,6 +2,6 @@ package com.ticketblock.exception;
 
 public class InvalidRoleException extends RuntimeException {
     public InvalidRoleException() {
-        super("Role must be one of [ROLE_USER, ROLE_ADMIN]");
+        super("Role must be one of [USER, ORGANIZER]");
     }
 }
