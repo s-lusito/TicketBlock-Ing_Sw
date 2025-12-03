33a5f2411183e5369ff6d1a425c89c57d49384be
samuele_lusito <samuelelusito@gmail.com>
2025-12-03 23:49:17 +0100

    feat(be): implement SecurityService to retrieve the currently logged-in user

---
+++ backend/src/main/java/com/ticketblock/service/SecurityService.java
@@ -0,0 +1,27 @@
+package com.ticketblock.service;
+
+import com.ticketblock.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class SecurityService {
+
+
+    public User getLoggedInUser(){
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated()) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        if (principal instanceof User) {
+            return (User) principal;
+        }
+        throw  new RuntimeException("Logged in user not found");
+    }
+
+}
