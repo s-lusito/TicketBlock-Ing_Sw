5d322bfb4adb1427e41ec19a8492329e0101ba38
samuele_lusito <samuelelusito@gmail.com>
2025-12-06 22:50:07 +0100

    fix(be): add logging for venue retrieval in VenueController

--- backend/src/main/java/com/ticketblock/controller/VenueController.java
+++ backend/src/main/java/com/ticketblock/controller/VenueController.java
@@ -3,6 +3,7 @@ package com.ticketblock.controller;
 import com.ticketblock.dto.Response.VenueDto;
 import com.ticketblock.service.VenueService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/venues")
+@Slf4j
 public class VenueController {
 
     private final VenueService venueService;
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getVenueById(@PathVariable Integer id) {
+        log.debug("Getting venue with id: {}", id);
         VenueDto venue = venueService.getVenueByVenueId(id);
         return ResponseEntity.ok(venue);
 
