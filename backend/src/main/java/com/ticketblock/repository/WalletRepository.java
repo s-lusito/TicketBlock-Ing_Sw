5f501211299c2c6effabeea6288e5e586922515c
EnZy46 <rubinoenzo46@gmail.com>
2025-12-22 18:06:49 +0100

    feat(be): add method to retrieve first available wallet in WalletRepository

--- backend/src/main/java/com/ticketblock/repository/WalletRepository.java
+++ backend/src/main/java/com/ticketblock/repository/WalletRepository.java
@@ -3,7 +3,8 @@ package com.ticketblock.repository;
 import com.ticketblock.entity.Seat;
 import com.ticketblock.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WalletRepository extends JpaRepository<Wallet, Integer> {
-
+    Wallet findFirstByFreeTrue();
 }
